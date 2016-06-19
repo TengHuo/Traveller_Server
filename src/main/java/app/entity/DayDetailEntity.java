a798b71c378be6fd7a0ef41c6713cb911d993410
jiayeZhu <1352953@tongji.edu.cn>
2016-06-19 12:01:16 +0800

    增加三个表的映射实体

---
+++ src/main/java/app/entity/DayDetailEntity.java
@@ -0,0 +1,148 @@
+package app.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by zhujay on 16/6/19.
+ */
+@Entity
+@Table(name = "day_detail", schema = "traveller", catalog = "")
+public class DayDetailEntity {
+    private String id;
+    private String planId;
+    private String postId;
+    private String startTime;
+    private String endTime;
+    private String destination;
+    private double latitude;
+    private double longitude;
+    private int type;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "plan_id")
+    public String getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    @Basic
+    @Column(name = "post_id")
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    @Basic
+    @Column(name = "start_time")
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    @Basic
+    @Column(name = "end_time")
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    @Basic
+    @Column(name = "destination")
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Basic
+    @Column(name = "latitude")
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Basic
+    @Column(name = "longitude")
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Basic
+    @Column(name = "type")
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DayDetailEntity that = (DayDetailEntity) o;
+
+        if (Double.compare(that.latitude, latitude) != 0) return false;
+        if (Double.compare(that.longitude, longitude) != 0) return false;
+        if (type != that.type) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (planId != null ? !planId.equals(that.planId) : that.planId != null) return false;
+        if (postId != null ? !postId.equals(that.postId) : that.postId != null) return false;
+        if (startTime != null ? !startTime.equals(that.startTime) : that.startTime != null) return false;
+        if (endTime != null ? !endTime.equals(that.endTime) : that.endTime != null) return false;
+        if (destination != null ? !destination.equals(that.destination) : that.destination != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (planId != null ? planId.hashCode() : 0);
+        result = 31 * result + (postId != null ? postId.hashCode() : 0);
+        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
+        result = 31 * result + (endTime != null ? endTime.hashCode() : 0);
+        result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        temp = Double.doubleToLongBits(latitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + type;
+        return result;
+    }
+}
---
+++ src/main/java/app/entity/PlanEntity.java
@@ -0,0 +1,80 @@
+package app.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+/**
+ * Created by zhujay on 16/6/19.
+ */
+@Entity
+@Table(name = "plan", schema = "traveller", catalog = "")
+public class PlanEntity {
+    private String planId;
+    private String scheduleId;
+    private Date travelDate;
+    private String plan;
+
+    @Id
+    @Column(name = "plan_id")
+    public String getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    @Basic
+    @Column(name = "schedule_id")
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    @Basic
+    @Column(name = "travel_date")
+    public Date getTravelDate() {
+        return travelDate;
+    }
+
+    public void setTravelDate(Date travelDate) {
+        this.travelDate = travelDate;
+    }
+
+    @Basic
+    @Column(name = "plan")
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlanEntity that = (PlanEntity) o;
+
+        if (planId != null ? !planId.equals(that.planId) : that.planId != null) return false;
+        if (scheduleId != null ? !scheduleId.equals(that.scheduleId) : that.scheduleId != null) return false;
+        if (travelDate != null ? !travelDate.equals(that.travelDate) : that.travelDate != null) return false;
+        if (plan != null ? !plan.equals(that.plan) : that.plan != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = planId != null ? planId.hashCode() : 0;
+        result = 31 * result + (scheduleId != null ? scheduleId.hashCode() : 0);
+        result = 31 * result + (travelDate != null ? travelDate.hashCode() : 0);
+        result = 31 * result + (plan != null ? plan.hashCode() : 0);
+        return result;
+    }
+}
